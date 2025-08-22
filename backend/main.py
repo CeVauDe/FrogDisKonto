f694fd45c87b61eca1afd136f794ca13f51b3d5b
CeVauDe <cevaude@outlook.de>
2025-08-22 22:19:48 +0200

    Add initial backend module with sample api call

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+def main():
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=os.environ["OPENROUTER_API_KEY"],
+    )
+
+
+    completion = client.chat.completions.create(
+        extra_body={},
+        model="openai/gpt-oss-20b:free",
+        messages=[{"role": "user", "content": "What is the meaning of life?"}],
+    )
+    print(completion.choices[0].message.content)
+
+if __name__ == "__main__":
+    main()
