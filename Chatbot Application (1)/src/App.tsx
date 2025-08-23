93c6ce82308b8ccd324aef118d363789ee62940a
CeVauDe <cevaude@outlook.de>
2025-08-23 13:54:30 +0200

    Remove unused files

--- Chatbot Application (1)/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatBot } from "./components/ChatBot";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-background">
-      <ChatBot />
-    </div>
-  );
-}
--- Chatbot Application/src/imports/Rectangle1.tsx
+++
@@ -1,9 +0,0 @@
-import microphoneIcon from 'figma:asset/599c9c136cba84a047e74782d802f6b7c4ba9549.png';
-
-export default function Rectangle1() {
-  return (
-    <div className="size-full flex justify-end items-center">
-      <img src={microphoneIcon} alt="PostFinance SpendCast Microphone" className="h-full object-contain" />
-    </div>
-  );
-}
